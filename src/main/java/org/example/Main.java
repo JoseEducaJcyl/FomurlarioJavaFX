53eaf20960edefcd9494d15a0f8a1778f36c7317
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-05-08 10:22:41 +0200

    Ejercicio 10 del bloque 5 de programacion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Formulario ejemplo");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        Label nombreLabel = new Label("Nombre: ");
+        TextField nombreTextField = new TextField();
+        grid.add(nombreLabel, 0, 0);
+        grid.add(nombreTextField, 1, 0);
+
+        Label emailLabel = new Label("Email: ");
+        TextField emailTextField = new TextField();
+        grid.add(emailLabel, 0, 1);
+        grid.add(emailTextField, 1, 1);
+
+        Label contraseniaLabel = new Label("Contraseña: ");
+        PasswordField contraseniaTextField = new PasswordField();
+        grid.add(contraseniaLabel, 0, 2);
+        grid.add(contraseniaTextField, 1, 2);
+
+        Button enviarButton = new Button("Enviar");
+        grid.add(enviarButton, 1, 3);
+        enviarButton.setOnAction(e -> {
+           String nombre = nombreTextField.getText();
+           String email = emailTextField.getText();
+           String contrasenia = contraseniaTextField.getText();
+           if (nombre.isEmpty() || email.isEmpty() || contrasenia.isEmpty()) {
+               Alert alerta = new Alert(Alert.AlertType.ERROR);
+               alerta.setTitle("Campos obrigatos");
+               alerta.setHeaderText("Hay algun campo sin rellenar");
+               alerta.showAndWait();
+               return;
+           }
+           System.out.println("Nombre: " + nombre);
+           System.out.println("Email: " + email);
+           System.out.println("Contrasenia: " + contrasenia);
+        });
+
+        Scene scene = new Scene(grid);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
